f9307f9ede816f47af67d464b6b4347904aaef9f
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-02-19 15:51:43 -0400

    El menú mobile aparece y desaparece, además esta posicionado

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const navbar_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
+const buttom_menu = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile-menu');
+
 
 navbar_email.addEventListener('click', () => {
     desktop_menu.classList.toggle('desktop-menu--active');
+});
+
+buttom_menu.addEventListener('click', () => {
+    mobile_menu.classList.toggle('mobile-menu--active');
 });
