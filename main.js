721a9a00c7da62fabafdd3712fbea53a3d259a03
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2023-02-23 11:31:32 -0400

    Por fin a partir de JS se hizo la vinculación de diferentes productos

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const navbar_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
 const buttom_menu = document.querySelector('.menu');
 const mobile_menu = document.querySelector('.mobile-menu');
-
+const cards_container = document.querySelector('.cards-container');
 
 navbar_email.addEventListener('click', () => {
     desktop_menu.classList.toggle('desktop-menu--active');
@@ -11,3 +11,98 @@ navbar_email.addEventListener('click', () => {
 buttom_menu.addEventListener('click', () => {
     mobile_menu.classList.toggle('mobile-menu--active');
 });
+
+// Auto generate products cards
+const productList = [];
+productList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1200,
+    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1600,
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Seat',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push ({
+    name:'Tennis Montain Bike',
+    price: 2200,
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 800,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 600,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+    name:'Bicycle seat bag',
+    price: 876,
+    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+});
+
+/* <div class="product-card">
+    <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+    <div class="product-info">
+        <div>
+        <p>$120,00</p>
+        <p>Bike</p>
+        </div>
+        <figure>
+        <img src="./icons/bt_add_to_cart.svg" alt="">
+        </figure>
+    </div>
+</div> */
+
+for (product of productList) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image);
+
+    const productInfo = document.createElement('div');
+    productInfo.classList.add('product-info');
+
+    const productInfoText = document.createElement('div');
+    const productPrice = document.createElement('p');
+    productPrice.textContent = `$${product.price}`;
+    const productName = document.createElement('p');
+    productName.textContent = product.name;
+    const productFigure = document.createElement('figure');
+    const productImgCart = document.createElement('img');
+    productImgCart.setAttribute('src', './icons/bt_add_to_cart.svg');
+    productImgCart.setAttribute('alt', 'Add to cart');
+
+    productFigure.appendChild(productImgCart);
+
+    productInfoText.appendChild(productPrice);
+    productInfoText.appendChild(productName);
+
+    productInfo.appendChild(productInfoText);
+    productInfo.appendChild(productFigure);
+
+    productCard.appendChild(img);
+    productCard.appendChild(productInfo);
+
+    cards_container.appendChild(productCard);
+}
